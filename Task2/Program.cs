ca2e7606d8292c880e1cc3faa26167f4ca8a32a1
Nikita Nikita <jassie.jassie@yandex.ru>
2023-12-06 14:46:37 +0300

    Финальное задание 2 Модуля 8

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"\luft"; // Необходимо указать путь к каталогу
+        DirectoryInfo dirInfo = new(path);
+        Console.WriteLine($"Указанный каталог {dirInfo.Name} имеет размер {GetSize(dirInfo)} байт");
+
+        static long GetSize(DirectoryInfo dirInfoParam)
+        {
+            long size = 0;
+            foreach(var fi in dirInfoParam.GetFiles())
+            {
+                Console.WriteLine($"Файл {fi.Name} имеет размер {fi.Length} байт");
+                size += fi.Length;
+            }
+            foreach(var di in dirInfoParam.GetDirectories())
+            {
+                size += GetSize(di);
+            }
+            return size;
+        }
+    }
+}
