8a28ecd9eda841be2e24b0d3c31c4afe2d1e85f7
Nikita Nikita <jassie.jassie@yandex.ru>
2023-12-20 13:25:34 +0300

    Проблема

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.Serialization.Formatters.Binary;
+namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datFilePath = @"\Users\Никонов\OneDrive\Рабочий стол\Students.dat";
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (var fileStream = new FileStream(datFilePath, FileMode.Open))
+            {
+                try
+                {
+                    var students = (Student[])formatter.Deserialize(fileStream);
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine($"{student.Name}\n{student.Group}\n{student.DayOfBirth}");
+                        Console.WriteLine("\n");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
